e8cf0ff66a1f63c347f461de11e4556f0ff03c26
Daniel Riveros <danielriverosdigital@gmail.com>
2025-05-08 17:10:35 -0400

    Working on Performing API Request. Creating and Deleting An Article

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -21,26 +21,94 @@ test.beforeEach(async ({page}) =>{
   })
 
   //Important to remember the * caracter at the end of the end using wildcard.
-  await page.route('*/**/api/articles*', async route =>{
-    const response = await route.fetch()
-    const responseBody = await response.json()
-    responseBody.articles[0].title = "This is a test title"
-    responseBody.articles[0].description = "This is a test description"
-    
-    await route.fulfill({
-      body: JSON.stringify(responseBody)
-    })
-  })
+  //await page.route('*/**/api/articles*', async route =>{
+  //  const response = await route.fetch()
+  //  const responseBody = await response.json()
+  //  responseBody.articles[0].title = "This is a test title"
+  //  responseBody.articles[0].description = "This is a test description"
+  //  
+  //  await route.fulfill({
+  //    body: JSON.stringify(responseBody)
+  //  })
+  //})
 
   await page.goto('https://conduit.bondaracademy.com/')
-  //The next instruction was necesary to make the Mock work properly
   await page.waitForTimeout(1000)
+  await page.getByText('Sign in').click()
+  await page.getByRole('textbox', {name: "Email"}).fill('istqbcertified@gmail.com')
+  await page.getByRole('textbox', {name: "Password"}).fill('welcome1')
+  await page.getByRole('button').click()
+  //The next instruction was necesary to make the Mock work properly. It's like an exception.  
+
 })
 
-test('has title', async ({ page }) => {
+test('Trying First Steps', async ({ page }) => {
  // Expect a title in the Home Web Page
   await expect(page.locator('.navbar-brand')).toHaveText('conduit')
   await expect(page.locator('app-article-list h1').first()).toContainText('This is a test title')
   await expect(page.locator('app-article-list p').first()).toContainText('This is a test description')
   await page.waitForTimeout(1000)
 });
+
+test('Performe API Request', async ({ page }) => {
+  //Important to remember the * caracter at the end of the end using wildcard.
+  await page.route('*/**/api/articles*', async route =>{
+    const response = await route.fetch()
+    const responseBody = await response.json()
+    responseBody.articles[0].title = "This is a MOCK test title"
+    responseBody.articles[0].description = "This is a MOCK test description"
+    
+    await route.fulfill({
+      body: JSON.stringify(responseBody)
+    })
+  }) 
+
+  await page.getByText('Global Feed').click()
+  // Expect a title in the Home Web Page
+   await expect(page.locator('.navbar-brand')).toHaveText('conduit')
+   await expect(page.locator('app-article-list h1').first()).toContainText('This is a MOCK test title')
+   await expect(page.locator('app-article-list p').first()).toContainText('This is a MOCK test description')
+   await page.waitForTimeout(1000)
+
+ })
+
+ test('Create And Delete An Article', async({page, request}) =>{
+  
+  const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login',{
+    data: {
+      "user":{"email":"istqbcertified@gmail.com","password":"welcome1"}
+    }    
+  })
+  const responseBody = await response.json()    
+  //console.log(responseBody.user.token)
+  const accessToken = responseBody.user.token
+
+  const articleResponse = await request.post('https://conduit-api.bondaracademy.com/api/articles/', {
+    data: {
+      "article":{"title":"This is another article title","description":"This is another article about","body":"This is another article description","tagList":[]}
+    },
+    headers: {
+      'authorization': `Token ${accessToken}`
+    }
+  })
+
+  expect(articleResponse.status()).toEqual(201)
+
+  await page.waitForTimeout(1000)
+  await page.locator('.nav-link').getByText('Home').click()
+  await page.getByText('Global Feed').click()
+  await page.waitForTimeout(1000)
+  await page.getByText('This is another article title').click()
+  //await page.locator('app-article-list h1').getByText('This is another article title3').click()
+  await page.waitForTimeout(1000)
+  await page.getByRole('button',{name: "Delete Article"}).first().click()
+  //await page.locator('[class="btn btn-sm btn-outline-danger"]').getByText('Delete Article').first().click()
+  
+  await page.getByText('Global Feed').click()
+  
+  await expect(page.locator('app-article-list h1').first()).not.toContainText('This is another article title')
+
+  await page.getByText('Settings').click()
+  await page.getByText('Or click here to logout.').click()
+
+ })
