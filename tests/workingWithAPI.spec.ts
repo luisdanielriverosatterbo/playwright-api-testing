dc1ac790161f995d26df06fb0f849dca457bc929
Daniel Riveros <danielriverosdigital@gmail.com>
2025-05-08 11:58:25 -0400

    Working in Modifying API Response

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -20,6 +20,18 @@ test.beforeEach(async ({page}) =>{
     })
   })
 
+  //Important to remember the * caracter at the end of the end using wildcard.
+  await page.route('*/**/api/articles*', async route =>{
+    const response = await route.fetch()
+    const responseBody = await response.json()
+    responseBody.articles[0].title = "This is a test title"
+    responseBody.articles[0].description = "This is a test description"
+    
+    await route.fulfill({
+      body: JSON.stringify(responseBody)
+    })
+  })
+
   await page.goto('https://conduit.bondaracademy.com/')
   //The next instruction was necesary to make the Mock work properly
   await page.waitForTimeout(1000)
@@ -28,4 +40,7 @@ test.beforeEach(async ({page}) =>{
 test('has title', async ({ page }) => {
  // Expect a title in the Home Web Page
   await expect(page.locator('.navbar-brand')).toHaveText('conduit')
+  await expect(page.locator('app-article-list h1').first()).toContainText('This is a test title')
+  await expect(page.locator('app-article-list p').first()).toContainText('This is a test description')
+  await page.waitForTimeout(1000)
 });
